8f4c0d6c9cf1d7c85c19a0c0969d8519753b98bf
podnes <podnestrade@gmail.com>
2023-03-19 12:56:53 +0200

    improve queries

--- ahmo-api/src/message/message.controller.ts
+++ ahmo-api/src/message/message.controller.ts
@@ -1,15 +1,17 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Request } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Request, UseGuards } from "@nestjs/common";
 import { MessageService } from './message.service';
 import { CreateMessageDto } from './dto/create-message.dto';
 import { UpdateMessageDto } from './dto/update-message.dto';
+import { JwtAuthGuard } from "../auth/guards/jwt-auth.guard";
 
 @Controller('messages')
 export class MessageController {
   constructor(private readonly messageService: MessageService) {}
 
   @Post()
+  @UseGuards(JwtAuthGuard)
   create(@Body() createMessageDto: CreateMessageDto, @Request() req) {
-    return this.messageService.create(createMessageDto, req.user);
+    return this.messageService.create(createMessageDto, req.user.id);
   }
 
   @Get()
